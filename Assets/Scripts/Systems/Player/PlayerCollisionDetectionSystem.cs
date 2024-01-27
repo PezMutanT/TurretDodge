f3a306aa2fb47e65f4d02ae0e04cea312a8b8203
PezMutanT <pezmutant@gmail.com>
2024-01-27 13:17:09 +0100

    Basic player trigger event detection.

---
+++ Assets/Scripts/Systems/Player/PlayerCollisionDetectionSystem.cs
@@ -0,0 +1,39 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Physics;
+using UnityEngine;
+
+namespace Systems
+{
+    [UpdateInGroup(typeof(LateSimulationSystemGroup), OrderLast = true)]
+    public partial struct PlayerCollisionDetectionSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+        }
+
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state)
+        {
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var simulationSingleton = SystemAPI.GetSingleton<SimulationSingleton>();
+            state.Dependency = new PlayerCollisionDetectionJob
+            {
+
+            }.Schedule(simulationSingleton, state.Dependency);
+        }
+    }
+    
+    public struct PlayerCollisionDetectionJob : ITriggerEventsJob
+    {
+        public void Execute(TriggerEvent triggerEvent)
+        {
+            Debug.Log($"Trigger event: {triggerEvent.EntityA} and {triggerEvent.EntityB}");
+        }
+    }
+}
