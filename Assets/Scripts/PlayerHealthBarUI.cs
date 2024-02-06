bb3621c05d385a16c7c8b746466f41a89bb298b6
PezMutanT <pezmutant@gmail.com>
2024-02-06 12:41:36 +0100

    Player health bar.

---
+++ Assets/Scripts/PlayerHealthBarUI.cs
@@ -0,0 +1,55 @@
+﻿using Components;
+using Systems;
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UserInterface
+{
+    public class PlayerHealthBarUI : MonoBehaviour
+    {
+        [SerializeField] private Image _progressBarMask;
+        private PlayerHealthComponent _playerHealth;
+        private float _maxHealth;
+        private float _currentHealth;
+        private bool _isInitialized;
+
+        private void Awake()
+        {
+            _isInitialized = false;
+            
+            var playerHealthSystem = World.DefaultGameObjectInjectionWorld.GetExistingSystemManaged<PlayerHealthSystem>();
+            playerHealthSystem.Init(this);
+        }
+
+        public bool IsInitialized() => _isInitialized;
+
+        public bool HasPlayerHealthChanged(float newHealth) => newHealth != _currentHealth;
+
+        public void Init()
+        {
+            _isInitialized = true;
+            
+            EntityManager _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            EntityQuery entityQuery = _entityManager.CreateEntityQuery(new ComponentType[]
+            {
+                typeof(PlayerHealthComponent)
+            });
+            
+            _playerHealth = entityQuery.GetSingleton<PlayerHealthComponent>();
+            _maxHealth = _playerHealth.Value;
+        }
+
+        public void RefreshHealthValue(float newHealth)
+        {
+            if (_currentHealth == newHealth)
+            {
+                return;
+            }
+
+            _currentHealth = newHealth;
+
+            _progressBarMask.fillAmount = newHealth / _maxHealth;
+        }
+    }
+}
---
+++ Assets/Scripts/Systems/Player/PlayerHealthSystem.cs
@@ -0,0 +1,43 @@
+﻿using Components;
+using UserInterface;
+using Unity.Entities;
+
+namespace Systems
+{
+    public partial class PlayerHealthSystem : SystemBase
+    {
+        private PlayerHealthBarUI _playerHealthBarUI;
+
+        protected override void OnCreate()
+        {
+            Enabled = false;
+            
+            RequireForUpdate<PlayerHealthComponent>();
+        }
+
+        public void Init(PlayerHealthBarUI playerHealthBarUI)
+        {
+            _playerHealthBarUI = playerHealthBarUI;
+
+            Enabled = true;
+        }
+
+        protected override void OnUpdate()
+        {
+            if (!_playerHealthBarUI.IsInitialized())
+            {
+                _playerHealthBarUI.Init();
+            }
+            
+            foreach (var playerHealth in SystemAPI.Query<RefRO<PlayerHealthComponent>>())
+            {
+                if (!_playerHealthBarUI.HasPlayerHealthChanged(playerHealth.ValueRO.Value))
+                {
+                    continue;
+                }
+                
+                _playerHealthBarUI.RefreshHealthValue(playerHealth.ValueRO.Value);
+            }
+        }
+    }
+}
